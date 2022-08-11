af1af5e3ce25c8f469ddc4e271da27def313608f
Sam <sane10@ya.ru>
2022-08-11 20:40:55 +0300

    policy calculate minimum age check

--- renlife_b2b_test/age.py
+++ renlife_b2b_test/age.py
@@ -1,6 +1,7 @@
 import datetime
 import time
 
+import allure
 from selene.support.shared import browser
 
 from renlife_b2b_test.controls.policy_calculate_ui import FillPersonalData
@@ -17,49 +18,64 @@ if day < 10:
     day = str(0) + str(day)
 
 
-# def birth_date_element():
-#     browser.element('.collapsable-info-block__content [name="birth_date__0"')
-# import datetime
-#
-# date = datetime.date.today()
-# def my_date():
-#     year = date.year
-#     month = date.month
-#     day = date.day
-#
-# def age_18(age):
-#     year_age = date.year - age
-#     print(z)
-#     #date.day, date.month, date.year-age
-#
-# age_18(18)
-
-
 def age_minimal(age):
     year_age = date.year - age
-    my_date = date.day, month, year_age
-    #browser.element('.el-date-editor [name="birth_date__0"]').set(date).press_enter()
-    browser.element('.collapsable-info-block__content [name="birth_date__0"').set(my_date).press_enter()
-    FillPersonalData().submit()
-    passed_vibor()
-    time.sleep(1)
+    my_date = day, month, year_age
+    with allure.step(f'Минимальный возраст {my_date}'):
+        #browser.element('.el-date-editor [name="birth_date__0"]').set(date).press_enter()
+        browser.element('.collapsable-info-block__content [name="birth_date__0"').set(my_date).press_enter()
+        FillPersonalData().submit()
+        passed_vibor()
+        time.sleep(1)
 
 def age_18_minus_1_day(age):
     year_age = date.year - age
-    day_age = date.day - 1
+    day_age = day - 1
     my_date = day_age, month, year_age
-    #browser.element('.el-date-editor [name="birth_date__0"]').set(date).press_enter()
-    browser.element('.collapsable-info-block__content [name="birth_date__0"').set(my_date).press_enter()
-    FillPersonalData().submit()
-    passed_vibor()
-    time.sleep(1)
+    with allure.step(f'Минимальный возраст - 1 день {my_date}'):
+        #browser.element('.el-date-editor [name="birth_date__0"]').set(date).press_enter()
+        browser.element('.collapsable-info-block__content [name="birth_date__0"').set(my_date).press_enter()
+        FillPersonalData().submit()
+        passed_vibor()
+        time.sleep(1)
 
 def age_18_plus_1_day(age):
     year_age = date.year - age
-    day_age = date.day + 1
+    day_age = day + 1
     my_date = day_age, month, year_age
-    #browser.element('.el-date-editor [name="birth_date__0"]').set(date).press_enter()
-    browser.element('.collapsable-info-block__content [name="birth_date__0"').set(my_date).press_enter()
-    FillPersonalData().submit()
-    error_vibor()
-    time.sleep(1)
+    with allure.step(f'Минимальный возраст + 1 день {my_date}'):
+        #browser.element('.el-date-editor [name="birth_date__0"]').set(date).press_enter()
+        browser.element('.collapsable-info-block__content [name="birth_date__0"').set(my_date).press_enter()
+        FillPersonalData().submit()
+        error_vibor()
+        time.sleep(1)
+
+def age_max(age):
+    year_age = date.year - age - 1
+    day_age = day
+    my_date = day_age, month, year_age
+    with allure.step(f'Максимальный возраст {my_date}'):
+        browser.element('.collapsable-info-block__content [name="birth_date__0"').set(my_date).press_enter()
+        FillPersonalData().submit()
+        error_vibor()
+        time.sleep(1)
+
+def age_max_plus_1(age):
+    year_age = date.year - age - 1
+    day_age = day + 1
+    my_date = day_age, month, year_age
+    with allure.step(f'Максимальный возраст + 1 день {my_date}'):
+        browser.element('.collapsable-info-block__content [name="birth_date__0"').set(my_date).press_enter()
+        FillPersonalData().submit()
+        passed_vibor()
+        time.sleep(1)
+
+def age_max_minus_1(age):
+    year_age = date.year - age - 1
+    day_age = day - 1
+    my_date = day_age, month, year_age
+    with allure.step(f'Максимальный возраст - 1 день {my_date}'):
+        browser.element('.collapsable-info-block__content [name="birth_date__0"').set(my_date).press_enter()
+        FillPersonalData().submit()
+        error_vibor()
+        time.sleep(1)
--- tests/python/b2b2/investor/test_age.py
+++ tests/python/b2b2/investor/test_age.py
@@ -1,20 +1,45 @@
 from renlife_b2b_test import age
 from renlife_b2b_test.controls import policy_calculate_ui
 from renlife_b2b_test.controls.policy_calculate_ui import FillPersonalData
-from tests.python.b2b2.investor.vznos.investor_ubrir_min_max_vznos import precondition
+from tests.python.b2b2.investor.vznos.test_investor_ubrir_min_max_vznos import precondition, postcondition
 
+min_vozrast = 18
+max_vozrast = 85
+
+#min_vozrast
 def precondition_sum():
     precondition()
     policy_calculate_ui.UI(100000)
 
 def test_age_minimal():
     precondition_sum()
-    age.age_minimal(18)
+    age.age_minimal(min_vozrast)
+    postcondition()
 
 def test_age_minimal_minus_1():
     precondition_sum()
-    age.age_18_minus_1_day(18)
+    age.age_18_minus_1_day(min_vozrast)
+    postcondition()
 
 def test_age_minimal_plus_1():
     precondition_sum()
-    age.age_18_plus_1_day(18)
+    age.age_18_plus_1_day(min_vozrast)
+    postcondition()
+
+
+#max_vozrast
+def test_max_age():
+    precondition_sum()
+    age.age_max(max_vozrast)
+    postcondition()
+
+def test_max_age_plus_1():
+    precondition_sum()
+    age.age_max_plus_1(max_vozrast)
+    postcondition()
+
+def test_max_age_minus_1():
+    precondition_sum()
+    age.age_max_minus_1(max_vozrast)
+    postcondition()
+
